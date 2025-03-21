1173b225f6960ccdc02e399d432b13432de56ea7
Ayen Monasha <161911907+LioMessiForeverKing@users.noreply.github.com>
2025-03-21 16:17:51 -0400

    corrected some redundunt code.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Backstage IQ',
+  description: 'Your Music Is Blowing Up. Do You Know Where?',
 }
 
 export default function RootLayout({
